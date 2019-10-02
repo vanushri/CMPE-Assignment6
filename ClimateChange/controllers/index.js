ace35c35479ea0a8871e00b9b52d9165881c55ec
Vanushri <vanushri.rawat@sjsu.edu>
2019-10-01 18:36:40 -0700

    initial setup

--- ClimateChange/controllers/index.js
+++ ClimateChange/controllers/index.js
@@ -1,4 +1,7 @@
 module.exports.get_index = function(req, res, next)
 {
-	res.render('index');
+	//res.render('index');
+	
+	//removing some extra pages and navigating index to login
+	res.render('login');
 };
