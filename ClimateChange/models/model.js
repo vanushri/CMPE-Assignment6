f9e9e9e22997fb177125715d0c5de86ab01cc3a7
anjanimall <anju.mallam@gmail.com>
2019-10-23 20:47:25 -0700

    part of delete

--- ClimateChange/models/model.js
+++ ClimateChange/models/model.js
@@ -13,3 +13,21 @@ module.exports.get_data = function(req, res)
                         res.render('manageArticles', { "ArticleList" : docs });
                     });
 };
+
+
+module.exports.post_deletearticle = function(req, res) 
+{
+    var articleid = req.body.queries;
+    var db = req.db;
+    var collection = db.get('All_Articles');
+    collection.remove( { "ArtId" : articleid },
+                       function (err, doc) 
+                       {
+                           if (err) {
+                               res.send("Delete failed.");
+                           }
+                           else {
+                               res.send("Successfully deleted " + articleid);
+                           }
+                       });
+};
